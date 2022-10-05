f3432d67abfb8a6fdb9b5b00ab608072a889e1a5
guruadp <gurunandhanadp@gmail.com>
2022-10-04 23:33:07 -0400

    added print statments and updated actual velocity

--- app/main.cpp
+++ app/main.cpp
@@ -13,12 +13,22 @@
 
 using namespace std;
 int main() {
-  PIDController controller(0.1, 0.5, 0.01);
-  double setpointVelocity = 10;
+  PIDController controller(0.1, 0.01, 0.5);
+  double setpointVelocity = 0;
   double actualVelocity = 0;
-  for (int i = 0; i < 50; i++) {
+
+  std::cout<<"Enter the actual velocity: "<<std::endl;
+  std::cin>>actualVelocity;
+  std::cout<<"Enter the desired velocity"<<std::endl;
+  std::cin>>setpointVelocity;
+
+  for (int i = 0; i < 10; i++) {
     double newVelocity =
         controller.computeVelocity(setpointVelocity, actualVelocity);
+    std::cout << "Actual : " << actualVelocity<< "   "
+              << "New Velocity : " << newVelocity << std::endl;
+    actualVelocity += newVelocity;
   }
+  
   return 0;
 }
