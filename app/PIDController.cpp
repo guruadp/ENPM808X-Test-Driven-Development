ac90db6cc30d1046eafc0b9f7ca8bfca02f509ac
Arshad-Engineer <arsh402@gmail.com>
2022-10-05 14:48:36 -0400

    Updated PIDController Comput method implementation.

--- app/PIDController.cpp
+++ app/PIDController.cpp
@@ -11,11 +11,30 @@
 
 #include "../include/PIDController.hpp"
 
-PIDController::PIDController(double kp, double ki, double kd) {}
+PIDController::PIDController(double p, double i, double d) {
+  kp = p;
+  ki = i;
+  kd = d;
+  dt = 0.1;
+  err_pre = 0;
+  err_sum = 0;
+}
 
 PIDController::~PIDController() {}
 
 double PIDController::computeVelocity(double setPoint, double actualVelocity) {
-  double output = 5;
-  return output;
+  // calculate error
+  double err = setPoint - actualVelocity;
+  // Proportional Term
+  double propTerm = kp * err;
+  // Integral Term
+  err_sum +=  ki*(err * dt);
+  // Derivative Term
+  double dervTerm = kd*(err - err_pre) / dt;
+  // Calculate total output
+  double computedVelocity = actualVelocity + propTerm + err_sum + dervTerm;
+  // Save error to previous error
+  err_pre = err;
+
+  return computedVelocity;
 }
